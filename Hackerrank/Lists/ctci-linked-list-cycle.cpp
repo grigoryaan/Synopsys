b07e30ebe88e1532f6538f47f923c320317a4e4d
grigoryaan <Grigoryanalen142@gmail.com>
2025-10-28 22:35:29 +0400

    Implement cycle detection in linked list

---
+++ Hackerrank/Lists/ctci-linked-list-cycle.cpp
@@ -0,0 +1,15 @@
+
+bool has_cycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
+}
