7995e0e3d83f5cff5177acc56284677988af1e20
grigoryaan <Grigoryanalen142@gmail.com>
2025-10-29 19:28:44 +0400

    Create Minimal-double.cpp

---
+++ Minimal-double.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+     long long bits = 0x0010000000000000; // double-i amenapoqr normalacvac drakan tivy
+    double min_d = *reinterpret_cast<double*>(&bits); // bitery poxum enq sarqum double tiv
+
+    std::cout << "minimal double =  " << min_d << std::endl;
+}
