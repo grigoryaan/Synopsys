bac92e689df61eeaff731cc3506ffc8e46346c3e
grigoryaan <Grigoryanalen142@gmail.com>
2026-04-05 22:29:37 +0400

    Update parser.h

--- OOP/statement_ast_compiler/parser.h
+++ OOP/statement_ast_compiler/parser.h
@@ -1,28 +1,19 @@
 #pragma once
 #include "ast.h"
-#include <vector>
-#include <string>
 
-struct Token {
-    int type;
-    std::string value;
-};
+// ── Expression parser 
+NodePtr parseExpression();
+NodePtr parseTerm();
+NodePtr parseFactor();
+NodePtr parseComparison();   
 
-class Parser {
-public:
-    Parser(const vector<Token>& tokens);
+// ── Statement parser 
+NodePtr parseStatement();    
+NodePtr parseIfStmt();
+NodePtr parseWhileStmt();
+NodePtr parseAssignOrExpr(); 
+NodePtr parseBlock();      
+NodePtr parseReturn();
 
-    shared_ptr<Node> parse();
 
-private:
-    vector<Token> tokens;
-    int pos;
-
-    shared_ptr<Node> parseExpression();
-    shared_ptr<Node> parseTerm();
-    shared_ptr<Node> parseFactor();
-
-    shared_ptr<Node> parseStatement();
-    shared_ptr<Node> parseIf();
-    shared_ptr<Node> parseWhile();
-};
+NodePtr parseProgram();
