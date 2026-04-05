20091cdf249fb7683725f8a980e53ad35d6b7ed6
grigoryaan <Grigoryanalen142@gmail.com>
2026-04-05 22:33:00 +0400

    Update ir.h

--- OOP/statement_ast_compiler/ir.h
+++ OOP/statement_ast_compiler/ir.h
@@ -1,19 +1,31 @@
 #pragma once
+#include "ast.h"
 #include <vector>
+#include <map>
+#include <string>
 
 enum class OpCode {
     LOAD_CONST,
     LOAD_VAR,
+    STORE_VAR,
     ADD, SUB, MUL, DIV,
-    CMP,
-    JMP,
-    JMP_IF_FALSE
+    CMP_LT, CMP_GT, CMP_LE, CMP_GE, CMP_EQ, CMP_NEQ,
+    JMP,       
+    JZ,        
+    NOP       
 };
 
 struct Instruction {
     OpCode op;
-    int left;
-    int right;
-    int dest;
-    double value;
+    int    left  = -1;
+    int    right = -1;
+    int    dest  = -1;   
+    double value = 0.0;
+    std::string varName;
 };
+
+
+int generate(NodePtr node, std::vector<Instruction>& code);
+
+double execute(std::vector<Instruction>& code,
+               std::map<std::string, double>& vars);
